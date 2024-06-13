63ad2a4b2665ecc0d379cfc05bab9ccf6b78e7a6
Monoji77 <chrisyong2009@live.com>
2024-06-14 06:15:43 +0800

    Major Change In Database
    1. Added Primary Keys to all (except combination tables since we might be rermoving this table if we're not using it)
    2. Extensive addition of primary major requirements for Data Science and Analytics
    3. Written complex queries necessary for our front-end involving information retrieval involving current date to obtain folders of relevant semesters for progress tracking, populating sample plans for Data science primary major (including CHS curriculum)
    4. Added CHS requirements
    5. Changed folder_name data type from 'Y1S1', 'Y1S2', ..., 'Y4S2' to TINYINT from 1-8 which maps appropriately to the semesters => allow for easier manipulation in complex queries
    7. Test cases included for testing in COMPLEX QUERIES-based sql files
    8. Removed SecondMinorRequirements table since it is the same as FirstMinorRequirements table
    
    Current Limitations needed to address:
    1. Either obtain user's matric number or user input whether the last digit of their matric number is odd/even for synchronised loading of data science primary major sample plan.

--- CREATE tables.sql
+++ CREATE tables.sql
@@ -1,37 +1,39 @@
--- CREATE TABLES --
+-- CREATE TABLES -- (Added primary key for all tables to ensure integrity of tables)
 
--- Folders Table
+-- Folders Table (changed folder_name to TINYINT for easier manipulation)
 CREATE TABLE Folders (
-	email VARCHAR(8) NOT NULL,
-    folder_name ENUM("Y1S1", "Y1S2", "Y2S1", "Y2S2","Y3S1", "Y3S2", "Y4S1", "Y4S2") NOT NULL, 
-	module_code VARCHAR(7) NOT NULL,
-    completion_status BOOLEAN NOT NULL
+	email VARCHAR(255) NOT NULL,
+    folder_name TINYINT NOT NULL, 
+	module_code VARCHAR(8) NOT NULL,
+    
+    -- all 3 required to be primary key since some mods are year long with the same module code
+    PRIMARY KEY (email, folder_name, module_code) 
 );
 
 -- Primary Major Requirement Table
 CREATE TABLE PrimaryMajorRequirements (
     primary_major VARCHAR(45),
-    module_type ENUM("Core", "GE"),
-    module_code VARCHAR(7)
+    module_type VARCHAR(45),
+    module_code VARCHAR(8),
+    
+    PRIMARY KEY (primary_major, module_code)
 );
 
 -- Secondary Major Requirements table
 CREATE TABLE SecondaryMajorRequirements (
     secondary_major VARCHAR(45),
-    module_code VARCHAR(7)
+    module_type VARCHAR(20),
+    module_code VARCHAR(8),
+    
+    PRIMARY KEY (secondary_major, module_code)
 );
 
 -- First Minor Requirements Table
 CREATE TABLE FirstMinorRequirements (
     first_minor VARCHAR(45),
-    module_code VARCHAR(7)
-);
-
--- Second Minor Requirements Table
-CREATE TABLE SecondMinorRequirements (
-    second_minor VARCHAR(45),
-    module_code VARCHAR(7)
-
+    module_code VARCHAR(8),
+    
+    PRIMARY KEY (first_minor, module_code)
 );
 
 -- Possible combinations of majors table (keep this table for autopopulating modules: Chris)
@@ -53,7 +55,7 @@ CREATE TABLE Users (
     reset_otp_exp BIGINT,
     completed_onboard BOOLEAN,
     completed_tutorial BOOLEAN,
-    enrolment_year VARCHAR(7),
+    enrolment_year VARCHAR(9),
     primary_major VARCHAR(45) NOT NULL,
     secondary_major VARCHAR(45),
     first_minor  VARCHAR(45),
@@ -63,3 +65,25 @@ CREATE TABLE Users (
     
     PRIMARY KEY (email)
 );
+
+/*
+-- Single Major Combination Sample Plans:
+	This table contains sample plans for users who do not secondary major, nor any minors.
+	
+    editor note: removed module_type since that is unique to each major (eg. CS2030 is CORE for Info Sys but UE for DSA majors). If we want to obtain module_type, we perform a join with the user's primary major.
+*/
+CREATE TABLE SingleMajorSamplePlan ( 
+	primary_major VARCHAR(45) NOT NULL,
+    folder_name TINYINT NOT NULL,
+    module_code VARCHAR(8) NOT NULL,
+    major_condition VARCHAR(8),
+    
+    PRIMARY KEY (primary_major, folder_name, module_code)
+);
+
+CREATE TABLE CHSRequirements (
+	pillar VARCHAR(45) NOT NULL,
+    module_code VARCHAR(45) NOT NULL,
+	
+    PRIMARY KEY (pillar, module_code)
+)
